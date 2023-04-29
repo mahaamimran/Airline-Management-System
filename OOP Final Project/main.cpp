552844a52d7a9380efdb3c4549691878bfa9b345
mahaamimran <mahaamimran@gmail.com>
2023-04-29 23:25:08 +0500

    namespace standard

--- OOP Final Project/main.cpp
+++ OOP Final Project/main.cpp
@@ -1,15 +1,7 @@
-//
-//  main.cpp
-//  OOP Final Project
-//
-//  Created by Maham Imran on 29/04/2023.
-//
-
+// final project
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    cout<<"i made a change hehehe\n";
+using namespace std;
+int main(){
+    cout<<"test\n";
     return 0;
 }
